542c17484c0ce0c95f46b00fb59f0ba3938d6c6e
osvaldx <romanonahuel24@gmail.com>
2025-09-28 01:36:25 -0300

    [+] se creo un custom-button para no repetir todo el tiempo estilado

--- src/app/components/custom-button/custom-button.ts
+++ src/app/components/custom-button/custom-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-custom-button',
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './custom-button.css'
 })
 export class CustomButton {
+
+  @Input() type?: string;
+  @Input() disabled?: boolean;
+  @Input() name?: string;
+  @Input() value?: string;
+  @Input() content?: string;
+  @Input() customColor?: boolean = false;
+  @Input() textColor: string = "";
+  @Input() backgroundColor: string = "";
+  @Input() href?: string;
+
+  public getStyles() {
+    let defaultStyle = "font-bold cursor-pointer normal-case px-4 py-2 rounded-md hover:scale-110 transition-transform duration-300 ";
+
+    if(this.customColor) {
+      defaultStyle += (this.backgroundColor?.trim()) ? this.backgroundColor + " " : "bg-gray-600 ";
+      defaultStyle += (this.textColor?.trim()) ? this.textColor + " " : "text-black ";
+    }
+
+    return defaultStyle
+  }
 
 }
