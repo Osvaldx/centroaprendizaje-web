37d3fe4b49d238c2b60a2f60a33f504875c0661c
osvaldx <romanonahuel24@gmail.com>
2025-09-28 01:37:26 -0300

    [+] se importo el componente card

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CustomButton } from '../../components/custom-button/custom-button';
+import { ServiceCard } from '../../components/service-card/service-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CustomButton, ServiceCard],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
