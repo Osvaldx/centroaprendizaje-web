62eefbbd144ebd6bcb90743052b1efd0dedd152a
Osvaldx <romanonahuel24@gmail.com>
2025-10-15 23:04:16 -0300

    [+] se hicieron grandes cambios en la localización como pidio el cliente

---
+++ src/app/components/location-section/location-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-section',
+  imports: [],
+  templateUrl: './location-section.html',
+  styleUrl: './location-section.css'
+})
+export class LocationSection {
+
+}
