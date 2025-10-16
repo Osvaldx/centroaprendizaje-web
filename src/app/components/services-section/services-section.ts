752f4d294cf1e1ffb1d30e1bbf89fd7ccac13285
Osvaldx <romanonahuel24@gmail.com>
2025-10-15 23:04:35 -0300

    [+] el cliente pidio modificar y agregar más informacion a los servicios

---
+++ src/app/components/services-section/services-section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ServiceCard } from '../service-card/service-card';
+
+@Component({
+  selector: 'app-services-section',
+  imports: [ServiceCard],
+  templateUrl: './services-section.html',
+  styleUrl: './services-section.css'
+})
+export class ServicesSection {
+
+}
