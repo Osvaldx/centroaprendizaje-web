22e454a62af99e7bbd6d9e55feee8a1d1f39603e
Osvaldx <romanonahuel24@gmail.com>
2025-10-15 23:01:14 -0300

    [+] se agrego DatePipe para el año

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './components/nav-bar/nav-bar';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('centroaprendizaje');
+  protected readonly today: number = Date.now();
 }
