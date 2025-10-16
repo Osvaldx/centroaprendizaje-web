979df0212bfb6c9c9b7c608dd6d3271bcef4eedc
Osvaldx <romanonahuel24@gmail.com>
2025-10-16 01:24:29 -0300

    [+] se agrego el carrusel al home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { HomeHero } from '../../components/home-hero/home-hero';
 import { AboutSection } from '../../components/about-section/about-section';
 import { LocationSection } from '../../components/location-section/location-section';
 import { ServicesSection } from '../../components/services-section/services-section';
+import { GalerySection } from '../../components/galery-section/galery-section';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHero, AboutSection, LocationSection, ServicesSection],
+  imports: [HomeHero, AboutSection, LocationSection, ServicesSection, GalerySection],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
