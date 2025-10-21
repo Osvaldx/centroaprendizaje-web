25927787aebee93f8e3ce1ad0093200c49571b0e
Osvaldx <romanonahuel24@gmail.com>
2025-10-20 21:11:50 -0300

    [+] se pidio ciertos dias y meses que haya promociones, se configuro para que la web detecte sola

--- src/app/components/services-section/services-section.ts
+++ src/app/components/services-section/services-section.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ServiceCard } from '../service-card/service-card';
 
 @Component({
@@ -7,6 +7,24 @@ import { ServiceCard } from '../service-card/service-card';
   templateUrl: './services-section.html',
   styleUrl: './services-section.css'
 })
-export class ServicesSection {
+export class ServicesSection implements OnInit {
+
+  public showPromo = signal<boolean>(false);
+
+  ngOnInit(): void {
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth() + 1; // +1 para hacerlo mes humano
+
+    const promoMonths = [4, 5, 6, 9, 10]; // abril, mayo, junio, septiembre, octubre
+
+    if(promoMonths.includes(month)) {
+      if(day >= 1 && day <= 10) {
+        this.showPromo.set(true);
+      } else {
+        this.showPromo.set(false);
+      }
+    }
+  }
 
 }
