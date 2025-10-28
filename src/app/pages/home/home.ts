9b54ef0d39add97f5cd040f22e93429a704f18d5
Osvaldx <romanonahuel24@gmail.com>
2025-10-28 15:24:30 -0300

    [+] se agrego el component de form

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,10 +4,11 @@ import { AboutSection } from '../../components/about-section/about-section';
 import { LocationSection } from '../../components/location-section/location-section';
 import { ServicesSection } from '../../components/services-section/services-section';
 import { GalerySection } from '../../components/galery-section/galery-section';
+import { FormSection } from '../../components/form-section/form-section';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHero, AboutSection, LocationSection, ServicesSection, GalerySection],
+  imports: [HomeHero, AboutSection, LocationSection, ServicesSection, GalerySection, FormSection],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
