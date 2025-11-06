2509ad7ba2c784f39c89bb6fa327dda9891449b5
Osvaldx <romanonahuel24@gmail.com>
2025-11-06 13:49:54 -0300

    [+] se configuro el carrusel y se saco el hecho desde 0

--- src/app/components/galery-section/galery-section.ts
+++ src/app/components/galery-section/galery-section.ts
@@ -1,14 +1,17 @@
-import { Component, OnDestroy, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { NgxMarqueeComponent, OmMarqueeItemDirective } from '@omnedia/ngx-marquee';
 
 @Component({
   selector: 'app-galery-section',
   standalone: true,
+  imports: [NgxMarqueeComponent, OmMarqueeItemDirective, CommonModule],
   templateUrl: './galery-section.html',
   styleUrl: './galery-section.css'
 })
-export class GalerySection implements OnDestroy {
+export class GalerySection {
 
-  images = signal([
+  images = [
     'assets/1.webp',
     'assets/2.webp',
     'assets/3.webp',
@@ -29,60 +32,5 @@ export class GalerySection implements OnDestroy {
     'assets/10.webp',
     'assets/11.webp',
     'assets/6.webp'
-  ]);
-
-  currentIndex = signal(0);
-  timeChangeIndex = signal(3);
-  private intervalId: any;
-
-  // Función para obtener las imágenes visibles (anterior, actual, siguiente)
-  getVisibleImages() {
-    const images = this.images();
-    const current = this.currentIndex();
-    const length = images.length;
-    
-    return {
-      prev: images[(current - 1 + length) % length],
-      current: images[current],
-      next: images[(current + 1) % length]
-    };
-  }
-
-  // Función para obtener el índice de una imagen
-  getImageIndex(offset: number) {
-    const length = this.images().length;
-    return (this.currentIndex() + offset + length) % length;
-  }
-
-  constructor() { 
-    const id = setInterval(() => {
-      this.timeChangeIndex.update(i => i-1);
-      if(this.timeChangeIndex() === 0) {
-        this.nextImage();
-      }
-    }, 1000);
-    
-    this.intervalId = id;
-  }
-
-  ngOnDestroy(): void {
-    clearInterval(this.intervalId);
-  }
-
-  nextImage() {
-    const next = (this.currentIndex() + 1) % this.images().length;
-    this.currentIndex.set(next);
-    this.timeChangeIndex.set(3);
-  }
-  
-  prevImage() {
-    const prev = (this.currentIndex() - 1 + this.images().length) % this.images().length;
-    this.currentIndex.set(prev);
-    this.timeChangeIndex.set(3);
-  }
-
-  goToImage(index: number) {
-    this.currentIndex.set(index);
-    this.timeChangeIndex.set(3);
-  }
+  ];
 }
