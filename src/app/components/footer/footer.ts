c15d321b64b6650143a6868663ab3ec9efe0db87
Osvaldx <romanonahuel24@gmail.com>
2025-11-06 12:45:11 -0300

    [+] dividimos el footer

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,14 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [DatePipe],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {
+
+  protected readonly today: number = Date.now();
+
+}
