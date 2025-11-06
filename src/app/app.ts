881bf158bd2ffdab44834d7044a7e2e99a9afe6b
Osvaldx <romanonahuel24@gmail.com>
2025-11-06 12:45:27 -0300

    [+] agregamos el footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './components/nav-bar/nav-bar';
-import { DatePipe } from '@angular/common';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, DatePipe],
+  imports: [RouterOutlet, NavBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('centroaprendizaje');
-  protected readonly today: number = Date.now();
 }
